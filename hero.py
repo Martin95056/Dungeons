03478b16caf170f2ab2d868afbe71ff433066718
Martin95056 <email_martin@abv.bg>
2015-12-15 22:51:19 +0200

    Fxed the FAIL. P.S. Little change to attack() method (and equip() one), described with #comments

--- hero.py
+++ hero.py
@@ -1,118 +1,12 @@
-from weapon import Weapon
-from spell import Spell
+from enemy import Enemy
 
 
-class Hero(object):
+class Hero(Enemy):
 	def __init__(self, name, title, health, mana, mana_regen):
 		self.name = name
 		self.title = title
-		self.health = health
-		self.mana = mana
 		self.mana_regen = mana_regen
-
-		self.curr_health = health
-		self.curr_mana = mana
-
-		self.phisical_damage = 0
-		self.magic_damage = 0
-		self.damage = 0
-
-		self.max_equiped_weapons = 0
-		self.max_learned_spells = 0
+		super().__init__(health, mana, 0)
 
 	def known_as(self):
 		return "{} the {}".format(self.name, self.title)
-
-	def get_health(self):
-		return self.curr_health
-
-	def get_mana(self):
-		return self.curr_mana
-	
-	def is_alive(self):
-		return self.curr_health > 0
-
-	def can_cast(self, spell):
-		try:
-			self.curr_mana > spell.mana_cost
-						
-			self.curr_mana -= spell.mana_cost
-			return True
-		
-		except:
-			raise Exception("NOT ENOUGH MANA!!!")
-
-	def take_damage(self, damage_points):
-		if damage_points >= self.curr_health:
-			self.curr_health = 0
-
-		else:
-			self.curr_health -= damage_points
-		return self.curr_health
-
-	def take_healing(self, healing_points):
-		if self.is_alive() == True:
-			if (self.curr_health + healing_points) <= self.health:
-				self.curr_health += healing_points
-				return True
-
-			else:
-				pass
-		else:
-			return False
-
-	def can_equip(self):
-		return self.max_equiped_weapons == 0
-
-	def can_learn_spell(self):
-		return self.max_learned_spells == 0
-
-	def equip(self, weapon):
-		try:
-			self.can_equip()
-
-			self.phisical_damage = weapon.damage
-			self.max_equiped_weapons = 1
-
-		except:
-			raise Exception("{} cannot carry anymore weapons.".format(self.known_as()))
-
-	def learn(self, spell):
-		try:
-			self.can_learn_spell()
-
-			self.magic_damage = spell.damage
-			self.max_learned_spells = 1
-
-		except:
-			raise Exception("{} cannot learn anymore magics.".format(self.known_as()))
-
-	def can_attack(self):
-		if self.max_equiped_weapons == 1 or self.max_learned_spells == 1:
-			return True
-
-		else:
-			return False
-
-	def attack(self, **kwargs):
-		for key in kwargs:
-
-			try:
-				if key == 'by' and kwargs[key] == 'weapon':
-					if self.can_attack() == True and self.phisical_damage != 0:
-						self.damage = self.phisical_damage
-
-					else:
-						self.damage = self.damage
-
-				elif key == 'by' and kwargs[key] == 'magic':
-					if self.can_attack() == True and self.magic_damage != 0:
-						self.damage = self.magic_damage
-
-					else:
-						self.damage = self.damage
-
-				return self.damage
-
-			except:
-				raise Exception('for key use {} and for keyworld use {} or {}, please.'.format('by', 'weapon', 'magic'))
